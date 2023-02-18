57afd62a6be4305e6b2feecd38aca07d72872ced
priya <priya@DESKTOP-H9JSVAH>
2023-02-18 12:12:55 +0530

    sprint2

--- EmployeeManagementSystem/src/main/java/com/employeemgmnt/repositories/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employeemgmnt.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employeemgmnt.entities.Employee;
-
-public interface CompanyRepository extends JpaRepository<Employee, Integer> {
-
-}
--- EmployeeManagementSystem/src/main/java/com/employeemgmnt/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employeemgmnt.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employeemgmnt.entities.Company;
-
-public interface EmployeeRepository extends JpaRepository<Company, Integer>{
-
-}
